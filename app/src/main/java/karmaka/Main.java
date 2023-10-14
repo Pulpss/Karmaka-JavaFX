dc440cc15091b808343d9ad9aff061e6591da00b
Pulpss <45542107+Pulpss@users.noreply.github.com>
2023-10-14 14:13:14 +0200

    feat: FXML addition, switched to Groovy

--- app/src/main/java/karmaka/Main.java
+++ app/src/main/java/karmaka/Main.java
@@ -1,7 +1,21 @@
 package karmaka;
-public class Main {
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent mainPage=FXMLLoader.load(getClass().getClassLoader().getResource("fxml/MainPage.fxml"));
+        Scene scene = new Scene(mainPage,1024,600);
+        primaryStage.setTitle("Plants VS Zombies");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     public static void main(String[] args) {
-        HelloFX.main(args);
+        launch(args);
     }
 }
