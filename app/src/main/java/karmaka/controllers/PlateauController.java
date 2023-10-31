ad17027f00937092d470a73797c3ccc3fe474f69
Pulpss <puulps@gmail.com>
2023-10-31 08:35:04 +0100

    feat: reorganized plateau

--- app/src/main/java/karmaka/controllers/PlateauController.java
+++ app/src/main/java/karmaka/controllers/PlateauController.java
@@ -12,11 +12,20 @@ import karmaka.classes.Partie;
 public class PlateauController implements Initializable {
     // Cards dimensions are 160x115
     @FXML
-    private ImageView source;
+    private ImageView source, fosse, adversaireDeck, adversaireOeuvres, adversaireVieFuture, deck, oeuvres, vieFuture,
+            main;
 
     @FXML
     public void initialize(URL arg0, ResourceBundle arg1) {
         System.out.println(Partie.getInstance().toString());
-        source.setImage(new Image("/images/cartes/Bassesse.png"));
+        source.setImage(new Image("/images/cartes/Back.png"));
+        fosse.setImage(new Image("/images/cartes/Bassesse.png"));
+        adversaireDeck.setImage(new Image("/images/cartes/Back.png"));
+        adversaireOeuvres.setImage(new Image("/images/cartes/Bassesse.png"));
+        adversaireVieFuture.setImage(new Image("/images/cartes/Bassesse.png"));
+        deck.setImage(new Image("/images/cartes/Back.png"));
+        oeuvres.setImage(new Image("/images/cartes/Bassesse.png"));
+        vieFuture.setImage(new Image("/images/cartes/Bassesse.png"));
+        main.setImage(new Image("/images/cartes/Bassesse.png"));
     }
 }
