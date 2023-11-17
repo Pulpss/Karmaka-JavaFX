ec7f4b8408baf2b6e64f092363dfe8d549117a00
Enguerrand LUCAT <enguerrand.lucat@gmail.com>
2023-11-17 22:08:39 +0100

    Bassesse

--- app/src/main/java/karmaka/classes/cartes/Bassesse.java
+++ app/src/main/java/karmaka/classes/cartes/Bassesse.java
@@ -2,13 +2,40 @@ package karmaka.classes.cartes;
 
 import karmaka.classes.Carte;
 import karmaka.classes.Couleur;
+import karmaka.classes.Partie;
+import karmaka.classes.piles.Fosse;
+import karmaka.classes.piles.Main;
+import karmaka.view.Router;
 
+import java.io.IOException;
+import java.util.Random;
 public class Bassesse extends Carte {
     public Bassesse() {
         super("Bassesse", Couleur.ROUGE, "Défaussez au hasard 2 cartes de la Main d’un rival.", 3);
     }
 
-    public void pouvoir() {
+    public void pouvoir() throws IOException{
+    	//TODO : tester
         System.out.println("Bassesse");
+        Random random = new Random();
+        Main mainAdv = Partie.getInstance().getJoueur((Partie.getInstance().getTour() + 1) % 2).getMain();
+        Fosse fosse = Partie.getInstance().getFosse();
+        if (mainAdv.size() > 1) {
+        Carte carte = mainAdv.getCartes().get(random.nextInt(mainAdv.size()));
+        mainAdv.piocher(carte);
+        fosse.ajouter(carte);
+        carte = mainAdv.getCartes().get(random.nextInt(mainAdv.size()));
+        mainAdv.piocher(carte);
+        fosse.ajouter(carte);
+        Router.getInstance().instructions("Deux cartes ont été défaussées !");
+        }
+        else if (mainAdv.size() == 1) {
+            fosse.ajouter(mainAdv.piocher());
+        Router.getInstance().instructions("Une carte a été défaussée !");
+        }
+        else {
+        	Router.getInstance().instructions("L'adversaire n'a pas de carte en main !");
+        }
+        Partie.getInstance().tourSuivant();
     }
 }
--- app/src/main/java/karmaka/classes/cartes/Duperie.java
+++ app/src/main/java/karmaka/classes/cartes/Duperie.java
@@ -1,14 +1,39 @@
 package karmaka.classes.cartes;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
 import karmaka.classes.Carte;
 import karmaka.classes.Couleur;
+import karmaka.classes.Partie;
+import karmaka.classes.piles.Main;
+import karmaka.view.Router;
 
 public class Duperie extends Carte {
     public Duperie() {
         super("Duperie", Couleur.BLEU, "Regardez 3 cartes de la Main d’un rival; ajoutez-en une à votre Main.", 3);
     }
 
-    public void pouvoir() {
+    public void pouvoir() throws IOException {
+        // TODO: tester
         System.out.println("Duperie");
+        Random random = new Random();
+        Main mainAdv = Partie.getInstance().getJoueur((Partie.getInstance().getTour() + 1) % 2).getMain();
+        Main main = Partie.getInstance().getJoueur(Partie.getInstance().getTour()).getMain();
+        ArrayList<Integer> randomPrec = new ArrayList<Integer>();
+        ArrayList<Carte> carteTemp = new ArrayList<Carte>();
+        for (int i = 0; i < Math.min(3, mainAdv.size()); i++) {
+            int newRandom = random.nextInt(mainAdv.size());
+            while (randomPrec.contains(newRandom)) {
+                newRandom = random.nextInt(mainAdv.size());
+            }
+            randomPrec.add(newRandom);
+            carteTemp.add(mainAdv.getCartes().get(newRandom));
+        }
+        Carte c = Router.getInstance().choix("Voici 3 cartes aléatoires de la main de votre adversaire, choisissez-en une.", carteTemp);
+        main.ajouter(mainAdv.piocher(c));
+        Router.getInstance().instructions("La carte a été ajoutée à votre main.");
+        Partie.getInstance().tourSuivant();
     }
 }
