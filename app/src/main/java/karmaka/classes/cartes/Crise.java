e5c00d6097a70eab1b0cfb655001fd8973091e00
Pulpss <puulps@gmail.com>
2023-11-22 16:27:07 +0100

    feat: carte crise

--- app/src/main/java/karmaka/classes/cartes/Crise.java
+++ app/src/main/java/karmaka/classes/cartes/Crise.java
@@ -1,14 +1,32 @@
 package karmaka.classes.cartes;
 
+import java.io.IOException;
+
 import karmaka.classes.Carte;
 import karmaka.classes.Couleur;
+import karmaka.classes.Partie;
+import karmaka.classes.piles.Fosse;
+import karmaka.classes.piles.Oeuvres;
+import karmaka.view.Router;
 
 public class Crise extends Carte {
     public Crise() {
         super("Crise", Couleur.ROUGE, "Le rival de votre choix défausse une de ses Oeuvres.", 2);
     }
 
-    public void pouvoir() {
+    public void pouvoir() throws IOException {
         System.out.println("Crise");
+        Oeuvres oeuvresAdv = Partie.getInstance().getJoueur((Partie.getInstance().getTour() + 1) % 2).getOeuvres();
+        Fosse fosse = Partie.getInstance().getFosse();
+        if (oeuvresAdv.size() > 0) {
+            Router.getInstance().setScene("plateauPlaceholder");
+            Router.getInstance().instructions("Laissez votre adversaire choisir une carte à défausser.");
+            Carte carte = Router.getInstance().choix("Veuillez choisir une carte à défausser de vos oeuvres.", oeuvresAdv.getCartes());
+            fosse.ajouter(oeuvresAdv.piocher(carte));
+            Router.getInstance().instructions("Une carte a été défaussée ! Redonnez la main à votre adversaire.");
+            Router.getInstance().setScene("plateau");
+        } else {
+            Router.getInstance().instructions("L'adversaire n'a pas d'oeuvres.");
+        }
     }
 }
--- app/src/main/java/karmaka/classes/cartes/Destinee.java
+++ app/src/main/java/karmaka/classes/cartes/Destinee.java
@@ -23,7 +23,7 @@ public class Destinee extends Carte {
         ArrayList<Carte> troisCartes = new ArrayList<Carte>(
                 source.getCartes().subList(Math.max(0, source.size() - 3 - 1), source.size() - 1));
         if (source.size() > 0) {
-            Router.getInstance().choix("Voici les 3 premières cartes de la source.", troisCartes);
+            Router.getInstance().afficher("Voici les 3 premières cartes de la source.", troisCartes);
             int choix = Integer.parseInt(Router.getInstance().choix(
                     "Combien de cartes voulez vous ajouter à votre vie future ?", "0", "0", "1",
                     "2"));
@@ -34,7 +34,7 @@ public class Destinee extends Carte {
                 troisCartes.remove(c);
             }
             for (int i = 0; i < troisCartes.size(); i++) {
-                Carte c = Router.getInstance().choix("Choisissez la carte à replacer dans la source en position" + i,
+                Carte c = Router.getInstance().choix("Choisissez la carte à replacer dans la source en position " + (i+1),
                         troisCartes);
                 source.ajouter(source.piocher(c));
                 troisCartes.remove(c);
